7009ff8cc5ac881b73e9d4bf4bd04811a4da7ebe
DiVouz <divouz@outlook.com>
2018-07-29 15:12:37 +0300

    new data

--- data.js
+++ data.js
@@ -1,13 +1,24 @@
 var data = {
 	"quotes" : [
-		{
-			"text" : "We all love you"
-		},
-		{
-			"text" : "We still want to love you"
-		},
-		{
-			"text" : "Love you"
-		}
+		{ "text" : "We love you" },
+		{ "text" : "I'm in love with you" },
+		{ "text" : "You are pretty" },
+		{ "text" : "You are awesome" },
+		{ "text" : "You are the best" },
+		{ "text" : "A hug?" },
+		{ "text" : "Kiss me" },
+		{ "text" : "Love, Laugh, Hug" },
+		{ "text" : "I fell in love with you" },
+		{ "text" : "I give you all of my love" },
+		{ "text" : "Life is beautiful" },
+		{ "text" : "Your smile is everything" },
+		{ "text" : "You are my favorite" },
+		{ "text" : "You are on my dreams" },
+		{ "text" : "I believe on you" },
+		{ "text" : "You are strong" },
+		{ "text" : "Don't forget self-love" },
+		{ "text" : "You are my paradise" },
+		{ "text" : "You are my choice" },
+		{ "text" : "Follow your dreams" }
 	]
 }
